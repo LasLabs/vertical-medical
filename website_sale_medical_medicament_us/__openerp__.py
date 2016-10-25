81a0fb6a6553fddc7209688682d090f90ed17f04
Oleg Bulkin <obulkin@laslabs.com>
2016-10-24 18:06:24 -0700

    [ADD] website_sale_medical_medicament_us: Brand/generic links * Extend the website_sale.product template to add links to brand-name medicament shop pages that point to equivalent generics and vice versa. These are based on the brand_ids and generic_ids fields on medical.medicament * Add demo data that showcases the change

---
+++ website_sale_medical_medicament_us/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Medicament Sales - Brand/Generic Links',
+    'summary': 'Links to brand/generic alternatives on medicament shop pages',
+    'version': '9.0.1.0.0',
+    'category': 'Medical',
+    'website': 'https://laslabs.com/',
+    'author': 'LasLabs, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'medical_medicament_us',
+        'website_sale_medical_medicament',
+    ],
+    'data': [
+        'views/website_sale_medical_medicament_us.xml',
+    ],
+    'demo': [
+        'demo/medical_medicament_gcn.xml',
+        'demo/medical_medicament.xml',
+    ],
+}
