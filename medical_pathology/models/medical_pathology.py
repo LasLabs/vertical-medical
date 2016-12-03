4b46b21236ecc8e2eac3ebe9791c35eb8df35831
Dave Lasley <dave@laslabs.com>
2016-12-03 09:36:11 -0800

    [IMP] medical_pathology: Improved code type handling * Add pathology code types to pathology categories * Add domains restricting pathologies and categories to that within the type * Update views to include default contexts and filters for code type * Add notes field to category + create an example demo record case from ICD-10 * Bump version * Add a parent child relationship on medical pathology * Update ReadMe * Add [DEMO] tag to all unique cols for demo data to avoid conflict with real data from importers * Add hyphenation to ICD code types

--- medical_pathology/models/medical_pathology.py
+++ medical_pathology/models/medical_pathology.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2004 Tech-Receptives
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError
 
 
 class MedicalPathology(models.Model):
     _name = 'medical.pathology'
     _description = 'Medical Pathology'
-    _sql_constraints = [
-        ('code_and_type_uniq',
-         'UNIQUE(code, code_type_id)',
-         'Pathology codes must be unique per Code Type.'),
-    ]
 
-    name = fields.Char(required=True, translate=True)
-    code = fields.Char(required=True)
+    name = fields.Char(
+        required=True,
+        translate=True,
+    )
+    code = fields.Char(
+        required=True,
+    )
     code_type_id = fields.Many2one(
         string='Code Type',
         comodel_name='medical.pathology.code.type',
+        index=True,
     )
-    notes = fields.Text(translate=True)
+    notes = fields.Text(
+        translate=True,
+    )
     category_id = fields.Many2one(
         string='Category of Pathology',
         comodel_name='medical.pathology.category',
+        domain="[('code_type_id', '=', code_type_id)]",
         index=True,
     )
     medical_pathology_group_ids = fields.Many2many(
@@ -31,9 +37,40 @@ class MedicalPathology(models.Model):
         comodel_name='medical.pathology.group',
         column1='pathology_id',
         colmun2='pathology_group_id',
-        relation="pathology_id_pathology_group_id_rel"
+        relation="pathology_id_pathology_group_id_rel",
     )
-    # @TODO: This should be in medical_genetics
-    protein = fields.Char(string='Protein Involved')
-    chromosome = fields.Char(string='Affected Chromosome')
-    gene = fields.Char(string='Affected Gene')
+    protein = fields.Char(
+        string='Protein Involved',
+    )
+    chromosome = fields.Char(
+        string='Affected Chromosome',
+    )
+    gene = fields.Char(
+        string='Affected Gene',
+    )
+    child_ids = fields.One2many(
+        string='Children Pathologies',
+        comodel_name='medical.pathology',
+        inverse_name='parent_id',
+        domain="[('code_type_id', '=', code_type_id)]",
+    )
+    parent_id = fields.Many2one(
+        string='Parent Pathology',
+        comodel_name='medical.pathology',
+        domain="[('code_type_id', '=', code_type_id)]",
+        index=True,
+    )
+
+    _sql_constraints = [
+        ('code_and_type_uniq',
+         'UNIQUE(code, code_type_id)',
+         'Pathology codes must be unique per Code Type.'),
+    ]
+
+    @api.multi
+    @api.constrains('parent_id')
+    def _check_recursion_parent_id(self):
+        if not self._check_recursion():
+            raise ValidationError(_(
+                'Error! You are attempting to create a recursive pathology.'
+            ))
