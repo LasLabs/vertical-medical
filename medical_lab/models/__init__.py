fcb4749c8ed2a6eec07195b60d53119129039720
Dave Lasley <dave@laslabs.com>
2016-12-05 12:53:21 -0800

    [MIG] medical_lab: Migrate oemedical_lab to v9 * Upgrade oemedical_lab to v9

---
+++ medical_lab/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import medical_lab_patient
+from . import medical_lab_test_criteria
+from . import medical_lab_test_result
+from . import medical_lab_test_type
+from . import medical_lab
+from . import medical_patient
