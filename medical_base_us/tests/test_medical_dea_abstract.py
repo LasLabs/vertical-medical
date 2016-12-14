226fd91326807e0c3b9188a5623bd21f89fdec9f
Oleg Bulkin <o.bulkin@gmail.com>
2016-12-14 13:29:14 -0800

    [FIX] medical_base_us: Unit tests (#142)
    * [FIX] medical_base_us: Unit tests
    * Fix unit tests in module by adding relevant test classes to registry,
    setting up their fields, and making a few other changes to the test logic

--- medical_base_us/tests/test_medical_dea_abstract.py
+++ medical_base_us/tests/test_medical_dea_abstract.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
-from openerp import models, fields, api
+from openerp import api, fields, models
 from openerp.exceptions import ValidationError
 
 
 class MedicalDeaAbstractTestMixer(TransactionCase):
 
-    def setUp(self, model_name='medical.abstract.dea'):
+    def setUp(self):
         super(MedicalDeaAbstractTestMixer, self).setUp()
-        self.model_obj = self.env[model_name]
+
+        MedicalTestDea._build_model(self.registry, self.cr)
+        self.model_obj = self.env[MedicalTestDea._name]
+        self.model_obj._prepare_setup()
+        self.model_obj._setup_base(False)
+        self.model_obj._setup_fields(False)
+        self.model_obj._setup_complete()
+
         self.valid = [
             'AP5836727',
         ]
         self.invalid = [
             'AP5836729',
+            'Invalid00',
         ]
         self.country_us = self.env['res.country'].search([
             ('code', '=', 'US'),
@@ -40,6 +48,7 @@ class MedicalTestDea(models.Model):
 class TestMedicalDeaAbstract(MedicalDeaAbstractTestMixer):
 
     def test_valid(self):
+        """ Test _dea_is_valid returns True if valid str input """
         for i in self.valid:
             self.assertTrue(
                 self.model_obj._dea_is_valid(i),
@@ -47,6 +56,7 @@ class TestMedicalDeaAbstract(MedicalDeaAbstractTestMixer):
             )
 
     def test_invalid(self):
+        """ Test _dea_is_valid returns False if invalid str input """
         for i in self.invalid:
             self.assertFalse(
                 self.model_obj._dea_is_valid(i),
@@ -54,30 +64,42 @@ class TestMedicalDeaAbstract(MedicalDeaAbstractTestMixer):
             )
 
     def test_false(self):
+        """ Test _dea_is_valid fails gracefully if given no/Falsey data """
         self.assertFalse(
             self.model_obj._dea_is_valid(False),
             'DEA validity check on False did not fail gracefully',
         )
 
     def test_constrain_valid_us(self):
-        self.assertTrue(
-            self.env['medical.test.dea'].create({
-                'ref': self.valid[0],
-                'country_id': self.country_us.id,
-            })
-        )
+        """ Test _dea_constrains_helper no ValidationError if valid ref """
+        test_model = self.env['medical.test.dea'].new({
+            'ref': self.valid[0],
+            'country_id': self.country_us.id,
+        })
+
+        try:
+            test_model._check_ref()
+        except ValidationError:
+            self.fail('A ValidationError was raised and should not have been.')
 
     def test_constrain_invalid_us(self):
+        """ Test _dea_constrains_helper raise ValidationError invalid ref """
+        test_model = self.env['medical.test.dea'].new({
+            'ref': self.invalid[0],
+            'country_id': self.country_us.id,
+        })
+
         with self.assertRaises(ValidationError):
-            self.env['medical.test.dea'].create({
-                'ref': self.invalid[0],
-                'country_id': self.country_us.id,
-            })
+            test_model._check_ref()
 
     def test_constrain_invalid_non_us(self):
-        self.assertTrue(
-            self.env['medical.test.dea'].create({
-                'ref': self.invalid[0],
-                'country_id': self.country_us.id + 1,
-            })
-        )
+        """ Test _dea_constrains_helper skips validation if not US """
+        test_model = self.env['medical.test.dea'].new({
+            'ref': self.invalid[0],
+            'country_id': self.country_us.id + 1,
+        })
+
+        try:
+            test_model._check_ref()
+        except ValidationError:
+            self.fail('A ValidationError was raised and should not have been.')
