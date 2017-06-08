eee89b02707b89d9a53dee8e99da196cd1eb68a7
gmeficent <gisela.mora@eficent.com>
2017-06-08 17:05:57 +0200

    [FIX][10.0] access incompatibility

--- medical_insurance/models/medical_insurance_plan.py
+++ medical_insurance/models/medical_insurance_plan.py
@@ -2,13 +2,19 @@
 # © 2015-TODAY LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class MedicalInsurancePlan(models.Model):
     _name = 'medical.insurance.plan'
     _description = 'Medical Insurance Providers'
-    _inherits = {'medical.insurance.template': 'insurance_template_id', }
+
+    name = fields.Char(
+        related='insurance_template_id.name',
+        stored=True,
+        readonly=True,
+        required=False,
+    )
     insurance_template_id = fields.Many2one(
         string='Plan Template',
         comodel_name='medical.insurance.template',
@@ -29,4 +35,41 @@ class MedicalInsurancePlan(models.Model):
     )
     member_exp = fields.Date(
         string='Expiration Date',
+    )
+    notes = fields.Text(
+        string='Extra Info',
+        help='Additional Information',
+    )
+
+    #Plan Template Information
+
+    plan_number = fields.Char(
+        related='insurance_template_id.plan_number',
+        stored=True,
+        readonly=True,
+        required=True,
+        help='Identification number for plan',
+    )
+    product_id = fields.Many2one(
+        related='insurance_template_id.product_id',
+        stored=True,
+        readonly=True,
+        string='Insurance Product',
+        comodel_name='product.product',
+        required=True,
+        ondelete='cascade',
+    )
+    insurance_company_id = fields.Many2one(
+        related='insurance_template_id.insurance_company_id',
+        stored=True,
+        readonly=True,
+        string='Insurance Provider',
+        comodel_name='medical.insurance.company',
+        help='Insurance Provider',
+    )
+    insurance_affiliation = fields.Selection(
+        related='insurance_template_id.insurance_affiliation',
+        stored=True,
+        readonly=True,
+        help='What type of entity is this insurance provided to?'
     )
