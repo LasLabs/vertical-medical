699431c1911eb129e0d9f20f886fa649dad45e5a
Brett Wood <bwood@laslabs.com>
2017-09-04 15:39:28 -0700

    [ADD] website_portals: Add js tours and unit tests.

--- website_portal_medical_prescription_order_line/tests/test_ui.py
+++ website_portal_medical_prescription_order_line/tests/test_ui.py
@@ -2,7 +2,7 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-from openerp.tests.common import HttpCase
+from odoo.tests.common import HttpCase
 
 
 class TestUi(HttpCase):
