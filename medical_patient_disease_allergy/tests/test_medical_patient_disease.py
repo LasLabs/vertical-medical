f1f60b02dcb499ed7b71d7b9d712107eb474090f
Oleg Bulkin <o.bulkin@gmail.com>
2017-10-20 14:37:57 -0700

    [REM] medical_patient_disease_allergy: Deprecate physician * Remove reference to medical.physician demo record that is no longer available * Update unit test to use new demo record ID

--- medical_patient_disease_allergy/tests/test_medical_patient_disease.py
+++ medical_patient_disease_allergy/tests/test_medical_patient_disease.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 LasLabs Inc.
+# Copyright 2016-2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from openerp.tests.common import TransactionCase
@@ -10,7 +10,7 @@ class TestMedicalPatientDisease(TransactionCase):
     def setUp(self):
         super(TestMedicalPatientDisease, self).setUp()
         self.disease_2 = self.env.ref(
-            'medical_patient_disease.medical_patient_disease_disease_2'
+            'medical_patient_disease.medical_patient_disease_2'
         )
         self.disease_3 = self.env.ref(
             'medical_patient_disease_allergy.medical_patient_disease_disease_3'
